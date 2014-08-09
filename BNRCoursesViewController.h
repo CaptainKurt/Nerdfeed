398c2b77d6d3b134293f0a87bb53871ca132bd08
CaptainKurt <walkkg12@wfu.edu>
2014-08-09 09:48:27 -0400

    Creation of Nerdfeed
    Nerdfeed is a Web Service based iPad application

---
+++ BNRCoursesViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNRCoursesViewController.h
+//  Nerdfeed
+//
+//  Created by Kurt Walker on 8/9/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BNRCoursesViewController : NSObject
+
+@end
---
+++ BNRCoursesViewController.m
@@ -0,0 +1,13 @@
+//
+//  BNRCoursesViewController.m
+//  Nerdfeed
+//
+//  Created by Kurt Walker on 8/9/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRCoursesViewController.h"
+
+@implementation BNRCoursesViewController
+
+@end
